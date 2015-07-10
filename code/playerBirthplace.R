24e829f6429f2b9d9effd802c32bb9dcb7eb1463
pssguy <agcur@rogers.com>
2015-07-10 13:26:54 -0700

    Acceptable birthplace code and output

--- code/playerBirthplace.R
+++ code/playerBirthplace.R
@@ -5,24 +5,26 @@ output$playerBirthplace <- renderLeaflet({
   if(is.null(input$playerA)) return()
   if(input$playerA=="") return()
   
-  if (!is.na(playerGame[playerGame$PLAYERID=="input$playerA",]$city[1])) {
+  #print(playerGame[playerGame$PLAYERID=="input$playerA",]$city[1])
+  
+  if (!is.na(playerGame[playerGame$PLAYERID==input$playerA,]$city[1])) {
   loc <- paste0(playerGame[playerGame$PLAYERID==input$playerA,]$city[1],", ",playerGame[playerGame$PLAYERID==input$playerA,]$COUNTRY[1])
   } else {
     loc <- playerGame[playerGame$PLAYERID==input$playerA,]$COUNTRY[1]
   }
-print(loc)
+#print(loc)
+  
+  
  
 df <- geocode(loc)
-print(str(df))
 
-theLat <- df$lat
-theLon <- df$lng
+ theLat <- df$lat
+ theLon <- df$lon
+
 
 df    %>%
   leaflet() %>%
- # 
   addTiles() %>% 
-#  setView(theLon,theLat, zoom=10) %>% 
-  clearBounds() %>% 
+  setView(theLon,theLat, zoom=9) %>% 
   addMarkers()
 })
