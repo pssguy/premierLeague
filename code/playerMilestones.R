3f449db6dd15280b931ac1c8ee33b70b1614a344
pssguy <agcur@rogers.com>
2015-08-18 10:13:05 -0700

    finalized playerMilestones - bulk of work in updateSQL file

--- code/playerMilestones.R
+++ code/playerMilestones.R
@@ -6,6 +6,6 @@ output$playerMilestones <- DT::renderDataTable({
   print("enter playerMilestones")
   print(glimpse(milestones))
 milestones %>% 
-    DT::datatable()
+  DT::datatable(rownames=FALSE,class='compact stripe hover row-border',options= list(paging = FALSE, searching = FALSE,info=FALSE))
   
 }) 
