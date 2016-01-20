f058a89a74a54fcbaf82831bf7d47c79d44b316c
pssguy <agcur@rogers.com>
2016-01-19 17:39:28 -0800

    parametized x axis title in player_ppg

--- code/player_ppg.R
+++ code/player_ppg.R
@@ -5,25 +5,24 @@ output$player_ppg <- renderPlotly({
   
   if(is.null(input$playerA)) return()
   
-  
-  # ppgPlayer <- playerGame %>%
-  #   filter(PLAYERID=="BENTD"&(START+subOn)>0) %>% 
-  #   mutate(gameOrder=row_number())
+ 
   
   ppgPlayer <- playerGame %>%
     filter(PLAYERID==input$playerA&(START+subOn)>0) %>% 
     mutate(gameOrder=row_number())
   
-  # games <- 
+   games <- nrow(ppgPlayer)
   
-  #xTitle <- paste0("Appearance Order - Total ",)
+  xTitle <- paste0("Appearance Order - Total ",games)
+
   
   plot_ly(ppgPlayer , x=plGameOrder, y=Gls, name="Goals", type="bar",
           hoverinfo="text",
           text=paste(TEAMNAME,"<br>v ",Opponents,"<br>",gameDate, "<br>Game ",gameOrder)) %>%
     add_trace(x=plGameOrder,y=Assists, name="Assists (inc secondary)")  %>%
     layout(hovermode = "closest", barmode="stack",
-           xaxis=list(title="Appearance Order"),
+         
+          xaxis=list(title=xTitle),
            yaxis=list(title="Points"),
            title=" Hover bar for details", titlefont=list(size=16)
     )
